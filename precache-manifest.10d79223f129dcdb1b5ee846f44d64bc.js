aa5121ef56fe81b854ac53672b77ad6b9060b85b
Phil Hendricks <pfhendri1@gmail.com>
2020-07-03 08:49:41 -0500

    Updates

---
+++ precache-manifest.10d79223f129dcdb1b5ee846f44d64bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7408d3bbf45658e765087654822793",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "55148cdc0f0b048cdc2f",
+    "url": "/monster-rolodex/static/css/main.83538c2d.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/monster-rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55148cdc0f0b048cdc2f",
+    "url": "/monster-rolodex/static/js/main.88b9c022.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
